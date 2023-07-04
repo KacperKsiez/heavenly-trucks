906d6af5e495e29dc27bfb59d4fbdcc5b56bf282
KacperKsiez <KacperKsiez@gmail.com>
2023-07-04 12:02:28 +0200

    starting slider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import './styles/main.scss';
 
 import { NavSwitcher } from './scripts/navSwitch';
+import { Splide } from '@splidejs/splide';
 
 new NavSwitcher().init();
+
+const slider = new Splide(document.querySelector('#slider'), {
+	lazyLoad: true,
+	perPage: 3,
+	autoHeight: true,
+});
+
+slider.mount();
